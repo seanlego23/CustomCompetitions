76ac6e3922b0f42fbafc4f18bc2910d96ffd6390
seanlego23 <seanlego23@gmail.com>
2021-08-01 22:00:41 -0400

    Initial commit

---
+++ src/main/java/io/github/seanlego23/customcompetitions/CustomCompetitions.java
@@ -0,0 +1,17 @@
+package io.github.seanlego23.customcompetitions;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CustomCompetitions extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
