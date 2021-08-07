5137045f293ee485d195f386c41bea4a59e8f5ae
seanlego23 <seanlego23@gmail.com>
2021-08-07 16:50:29 -0600

    Added Constructor

--- src/main/java/io/github/seanlego23/customcompetitions/CustomCompetitions.java
+++ src/main/java/io/github/seanlego23/customcompetitions/CustomCompetitions.java
@@ -4,6 +4,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CustomCompetitions extends JavaPlugin {
 
+    public CustomCompetitions() { }
+
     @Override
     public void onEnable() {
         // Plugin startup logic
