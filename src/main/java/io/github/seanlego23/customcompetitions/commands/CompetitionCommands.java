b22bbe41b5bd8aea6d8cb4fc051f78b6b9d4a484
seanlego23 <seanlego23@gmail.com>
2021-08-09 22:53:56 -0600

    Created CompetitionCommands.java

---
+++ src/main/java/io/github/seanlego23/customcompetitions/commands/CompetitionCommands.java
@@ -0,0 +1,18 @@
+package io.github.seanlego23.customcompetitions.commands;
+
+import com.sk89q.intake.Command;
+import com.sk89q.intake.Require;
+
+public class CompetitionCommands {
+
+    public CompetitionCommands() {
+
+    }
+
+    @Command(aliases = "create", desc = "")
+    @Require("customcompetitions.create")
+    public void create() {
+
+    }
+
+}
