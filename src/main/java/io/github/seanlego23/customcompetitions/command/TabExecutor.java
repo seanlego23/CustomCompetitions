b09319748c8d1aacafe907976872cd74e544b7d0
seanlego23 <seanlego23@gmail.com>
2021-08-09 22:38:15 -0600

    Deleted

--- src/main/java/io/github/seanlego23/customcompetitions/command/TabExecutor.java
+++
@@ -1,5 +0,0 @@
-package io.github.seanlego23.customcompetitions.command;
-
-public interface TabExecutor extends CommandExecutor, TabCompleter {
-
-}
--- src/main/java/io/github/seanlego23/customcompetitions/permissions/Permission.java
+++
@@ -1,4 +0,0 @@
-package io.github.seanlego23.customcompetitions.permissions;
-
-public enum Permission {
-}
