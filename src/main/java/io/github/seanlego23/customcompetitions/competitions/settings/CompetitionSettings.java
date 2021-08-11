992bf0f45e9c7e9697703147567b7beb7c1908c4
seanlego23 <seanlego23@gmail.com>
2021-08-11 11:38:39 -0600

    Added stages

--- src/main/java/io/github/seanlego23/customcompetitions/competitions/settings/CompetitionSettings.java
+++ src/main/java/io/github/seanlego23/customcompetitions/competitions/settings/CompetitionSettings.java
@@ -1,10 +1,13 @@
 package io.github.seanlego23.customcompetitions.competitions.settings;
 
+import java.util.List;
+
 public class CompetitionSettings {
 
     private CompetitionTimer before;
     private CompetitionTimer between;
     private boolean reoccur;
+    private List<CompetitionStage> stages;
 
     public CompetitionSettings() {
 
