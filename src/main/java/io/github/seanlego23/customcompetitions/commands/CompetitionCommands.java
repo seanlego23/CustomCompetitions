e6dbef68cacd3eb847d97162c599570f179affc5
seanlego23 <seanlego23@gmail.com>
2021-08-11 08:09:33 -0600

    Added create and info commands

--- src/main/java/io/github/seanlego23/customcompetitions/commands/CompetitionCommands.java
+++ src/main/java/io/github/seanlego23/customcompetitions/commands/CompetitionCommands.java
@@ -2,6 +2,9 @@ package io.github.seanlego23.customcompetitions.commands;
 
 import com.sk89q.intake.Command;
 import com.sk89q.intake.Require;
+import io.github.seanlego23.customcompetitions.competitions.Competition;
+import io.github.seanlego23.customcompetitions.competitions.settings.CompetitionSettings;
+import io.github.seanlego23.customcompetitions.user.User;
 
 public class CompetitionCommands {
 
@@ -9,9 +12,15 @@ public class CompetitionCommands {
 
     }
 
-    @Command(aliases = "create", desc = "")
+    @Command(aliases = "create", desc = "Create a custom competition.")
     @Require("customcompetitions.create")
-    public void create() {
+    public void create(User user, String name, CompetitionSettings settings) {
+
+    }
+
+    @Command(aliases = "info", desc = "Get info about a competition.")
+    @Require("customcompetitions.info")
+    public void info(User user, Competition competition) {
 
     }
 
