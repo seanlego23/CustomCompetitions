77ed987a9e51656ee410ac1186a084de5668207e
seanlego23 <seanlego23@gmail.com>
2021-08-11 08:08:34 -0600

    Created Competition.java

---
+++ src/main/java/io/github/seanlego23/customcompetitions/competitions/Competition.java
@@ -0,0 +1,16 @@
+package io.github.seanlego23.customcompetitions.competitions;
+
+import io.github.seanlego23.customcompetitions.competitions.settings.CompetitionSettings;
+import io.github.seanlego23.customcompetitions.user.User;
+
+public class Competition {
+
+    private String name;
+    private User creator;
+    private CompetitionSettings settings;
+
+    public Competition() {
+
+    }
+
+}
