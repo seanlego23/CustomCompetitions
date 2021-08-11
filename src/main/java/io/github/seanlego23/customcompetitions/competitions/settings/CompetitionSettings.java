324640a2a3ef2e571a3b01823905e752232caea3
seanlego23 <seanlego23@gmail.com>
2021-08-11 11:23:45 -0600

    Added timers

--- src/main/java/io/github/seanlego23/customcompetitions/competitions/settings/CompetitionSettings.java
+++ src/main/java/io/github/seanlego23/customcompetitions/competitions/settings/CompetitionSettings.java
@@ -2,6 +2,10 @@ package io.github.seanlego23.customcompetitions.competitions.settings;
 
 public class CompetitionSettings {
 
+    private CompetitionTimer before;
+    private CompetitionTimer between;
+    private boolean reoccur;
+
     public CompetitionSettings() {
 
     }
