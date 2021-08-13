5bbf860266c2514bc9d83185226cefa00d8ec2c4
seanlego23 <seanlego23@gmail.com>
2021-08-13 13:59:51 -0400

    Used MultiversePortals implementation

--- src/main/java/io/github/seanlego23/customcompetitions/dependencies/worldedit/WorldEditConnection.java
+++ src/main/java/io/github/seanlego23/customcompetitions/dependencies/worldedit/WorldEditConnection.java
@@ -1,9 +1,103 @@
 package io.github.seanlego23.customcompetitions.dependencies.worldedit;
 
+import com.sk89q.worldedit.IncompleteRegionException;
+import com.sk89q.worldedit.WorldEdit;
+import com.sk89q.worldedit.bukkit.BukkitPlayer;
+import com.sk89q.worldedit.bukkit.BukkitWorld;
+import com.sk89q.worldedit.bukkit.WorldEditPlugin;
+import com.sk89q.worldedit.regions.Region;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+
 public class WorldEditConnection {
 
-    public WorldEditConnection() {
+    private final Plugin connectingPlugin;
 
+    private WorldEditPlugin worldEditPlugin;
+    WorldEdit worldEdit;
+
+    WorldEditConnection(Plugin plugin) {
+        if (plugin == null) {
+            throw new RuntimeException("plugin must not be null.");
+        }
+        this.connectingPlugin = plugin;
     }
 
+    private WorldEditPlugin retrieveWorldEditPluginFromServer() {
+        Plugin plugin = connectingPlugin.getServer().getPluginManager().getPlugin("WorldEdit");
+        if (plugin == null) {
+            plugin = connectingPlugin.getServer().getPluginManager().getPlugin("FastAsyncWorldEdit");
+        }
+
+        if (plugin == null) {
+            return null;
+        } else if (plugin instanceof WorldEditPlugin) {
+            return (WorldEditPlugin)plugin;
+        } else {
+            connectingPlugin.getLogger().warning("WorldEdit v" + plugin.getDescription().getVersion()
+                                                 + " is incompatible with " +
+                                                 connectingPlugin.getDescription().getName() + " v"
+                                                 + connectingPlugin.getDescription().getVersion());
+            return null;
+        }
+    }
+
+    /**
+     * Attempts to connect to the WorldEdit plugin.
+     *
+     * @return true if the WorldEdit plugin is available and able to be interfaced with.
+     */
+    boolean connect() {
+        if (!isConnected()) {
+            worldEditPlugin = retrieveWorldEditPluginFromServer();
+            if (worldEditPlugin != null) {
+                this.worldEdit = worldEditPlugin.getWorldEdit();
+                connectingPlugin.getLogger()
+                                .info(String.format("Found %s. Using it for selections.", worldEditPlugin.getName()));
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void disconnect() {
+        worldEditPlugin = null;
+        this.worldEdit = null;
+    }
+
+    /**
+     * Tests the connection to the WorldEdit plugin.
+     *
+     * @return true if current connected to the WorldEdit plugin.
+     */
+    public boolean isConnected() {
+        return worldEditPlugin != null;
+    }
+
+    private Region getSelection(Player player) {
+        if (!isConnected()) {
+            throw new RuntimeException("WorldEdit connection is unavailable.");
+        }
+        try {
+            return worldEdit.getSessionManager().get(new BukkitPlayer(worldEditPlugin, player)).getSelection(
+                    new BukkitWorld(player.getWorld()));
+        } catch (IncompleteRegionException e) {
+            return null;
+        }
+    }
+
+    /**
+     * @return true if the player currently has a WorldEdit selection.
+     */
+    public boolean isSelectionAvailable(Player player) {
+        if (player == null) {
+            throw new RuntimeException("player must not be null.");
+        }
+
+        if (!isConnected()) {
+            throw new RuntimeException("WorldEdit connection is unavailable.");
+        }
+
+        return getSelection(player) != null;
+    }
 }
