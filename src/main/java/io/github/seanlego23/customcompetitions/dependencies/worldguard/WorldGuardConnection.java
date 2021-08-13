e734cf274ab2cac011bd49b92091d4ccb8f3cd58
seanlego23 <seanlego23@gmail.com>
2021-08-13 13:54:10 -0400

    Created WorldGuardConnection.java

---
+++ src/main/java/io/github/seanlego23/customcompetitions/dependencies/worldguard/WorldGuardConnection.java
@@ -0,0 +1,13 @@
+package io.github.seanlego23.customcompetitions.dependencies.worldguard;
+
+import io.github.seanlego23.customcompetitions.CustomCompetitions;
+
+public class WorldGuardConnection {
+
+    private final CustomCompetitions connectingPlugin;
+
+    public WorldGuardConnection(CustomCompetitions plugin) {
+        connectingPlugin = plugin;
+    }
+
+}
