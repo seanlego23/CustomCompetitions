96b7bdd2f66dd616d36a55f00a4b0a75a0245793
seanlego23 <seanlego23@gmail.com>
2021-08-13 17:04:01 -0400

    Modified WorldEditConnection for WorldGuard

--- src/main/java/io/github/seanlego23/customcompetitions/dependencies/worldguard/WorldGuardConnection.java
+++ src/main/java/io/github/seanlego23/customcompetitions/dependencies/worldguard/WorldGuardConnection.java
@@ -1,13 +1,67 @@
 package io.github.seanlego23.customcompetitions.dependencies.worldguard;
 
-import io.github.seanlego23.customcompetitions.CustomCompetitions;
+import com.sk89q.worldguard.WorldGuard;
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import org.bukkit.plugin.Plugin;
 
 public class WorldGuardConnection {
 
-    private final CustomCompetitions connectingPlugin;
+    private final Plugin connectingPlugin;
 
-    public WorldGuardConnection(CustomCompetitions plugin) {
+    private WorldGuardPlugin worldGuardPlugin;
+    WorldGuard worldGuard;
+
+    public WorldGuardConnection(Plugin plugin) {
         connectingPlugin = plugin;
+    }
+
+    private WorldGuardPlugin retrieveWorldGuardPluginFromServer() {
+        Plugin plugin = connectingPlugin.getServer().getPluginManager().getPlugin("WorldGuard");
+
+        if (plugin == null)
+            return null;
+        else if (plugin instanceof WorldGuardPlugin)
+            return (WorldGuardPlugin) plugin;
+        else {
+            connectingPlugin.getLogger().warning("WorldEdit v" + plugin.getDescription().getVersion()
+                                                 + " is incompatible with " +
+                                                 connectingPlugin.getDescription().getName() + " v"
+                                                 + connectingPlugin.getDescription().getVersion());
+            return null;
+        }
+    }
+
+    /**
+     * Attempts to connect to the WorldEdit plugin.
+     *
+     * @return true if the WorldEdit plugin is available and able to be interfaced with.
+     */
+    boolean connect() {
+        if (!isConnected()) {
+            worldGuardPlugin = retrieveWorldGuardPluginFromServer();
+            if (worldGuardPlugin != null) {
+                worldGuard = WorldGuard.getInstance();
+                connectingPlugin.getLogger()
+                                .info(String.format("Found %s. Using it for selections.", worldGuardPlugin.getName()));
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void disconnect() {
+        worldGuardPlugin = null;
+        worldGuard = null;
+    }
+
+    /**
+     * Tests the connection to the WorldEdit plugin.
+     *
+     * @return true if current connected to the WorldEdit plugin.
+     */
+    @SuppressWarnings("BooleanMethodIsAlwaysInverted")
+    public boolean isConnected() {
+        return worldGuardPlugin != null;
     }
 
 }
