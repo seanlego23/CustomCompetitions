c0f8ba516cef18a96cc6905457453a829971886b
seanlego23 <seanlego23@gmail.com>
2021-08-13 20:10:57 -0400

    Added WorldGuard plugin event listener

--- src/main/java/io/github/seanlego23/customcompetitions/dependencies/worldguard/WorldGuardConnection.java
+++ src/main/java/io/github/seanlego23/customcompetitions/dependencies/worldguard/WorldGuardConnection.java
@@ -2,7 +2,12 @@ package io.github.seanlego23.customcompetitions.dependencies.worldguard;
 
 import com.sk89q.worldguard.WorldGuard;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.server.PluginDisableEvent;
+import org.bukkit.event.server.PluginEnableEvent;
 import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
 
 public class WorldGuardConnection {
 
@@ -11,8 +16,9 @@ public class WorldGuardConnection {
     private WorldGuardPlugin worldGuardPlugin;
     WorldGuard worldGuard;
 
-    public WorldGuardConnection(Plugin plugin) {
+    public WorldGuardConnection(@NotNull Plugin plugin) {
         connectingPlugin = plugin;
+        plugin.getServer().getPluginManager().registerEvents(new WorldGuardListener(), plugin);
     }
 
     private WorldGuardPlugin retrieveWorldGuardPluginFromServer() {
@@ -31,22 +37,36 @@ public class WorldGuardConnection {
         }
     }
 
-    boolean connect() {
+    void connect() {
         if (!isConnected()) {
             worldGuardPlugin = retrieveWorldGuardPluginFromServer();
             if (worldGuardPlugin != null) {
                 worldGuard = WorldGuard.getInstance();
                 connectingPlugin.getLogger()
                                 .info(String.format("Found %s. Using it for selections.", worldGuardPlugin.getName()));
-                return true;
             }
         }
-        return false;
     }
 
     void disconnect() {
         worldGuardPlugin = null;
         worldGuard = null;
+    }
+
+    private class WorldGuardListener implements Listener {
+
+        @EventHandler
+        private void worldGuardEnabled(PluginEnableEvent event) {
+            if (event.getPlugin().getName().equals("WorldGuard"))
+                connect();
+        }
+
+        @EventHandler
+        private void worldGuardDisabled(PluginDisableEvent event) {
+            if (event.getPlugin().getName().equals("WorldGuard"))
+                disconnect();
+        }
+
     }
 
     @SuppressWarnings("BooleanMethodIsAlwaysInverted")
