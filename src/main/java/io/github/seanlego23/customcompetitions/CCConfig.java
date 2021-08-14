723013081adca310bbf9d989412faafb16f2c679
seanlego23 <seanlego23@gmail.com>
2021-08-14 13:22:42 -0400

    Added BiomeRegion options

--- src/main/java/io/github/seanlego23/customcompetitions/CCConfig.java
+++ src/main/java/io/github/seanlego23/customcompetitions/CCConfig.java
@@ -2,6 +2,9 @@ package io.github.seanlego23.customcompetitions;
 
 public class CCConfig {
 
+    private int maxBiomeRegionChunkSearch;
+    private int maxBiomeRegionChunks;
+
     public CCConfig() { }
 
 }
