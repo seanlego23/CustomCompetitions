c70976a0e1b6da446fb2081cfae8711f88fd05d5
seanlego23 <seanlego23@gmail.com>
2021-08-14 22:19:40 -0400

    Added config annotations

--- src/main/java/io/github/seanlego23/customcompetitions/config/CCConfig.java
+++ src/main/java/io/github/seanlego23/customcompetitions/config/CCConfig.java
@@ -1,9 +1,27 @@
 package io.github.seanlego23.customcompetitions.config;
 
+@ConfigOptions(name = "config",
+               header = """
+                        {{pre:-, post:-, flags:ceoh, info:{1, 1, 75}}, id:"CustomCompetitions Config"}
+                        Author: {id:author}
+                        Version: {id:version}
+                        {id:-, flags:p, info:{75}}
+                        """ + "\n",
+               sectionHeader = """
+                               {id:-, flags:p, info:{40}}
+                               {{pre:-, post:-, flags:ceoh, info:{1, 1, 40}}, id:section-name}
+                               {id:-, flags:p, info:{40}}
+                               """)
 public class CCConfig {
 
-    private int maxBiomeRegionChunkSearch;
-    private int maxBiomeRegionChunks;
+    @ConfigSection(name = "WorldEdit")
+    @Option(alias = "searchLength", section = "WorldEdit", path = "biomeRegion")
+    @OptionDescription(desc = "How many chunks in each direction to search before giving up.")
+    public int maxBiomeRegionChunkSearch;
+
+    @Option(alias = "maximumSize", section = "WorldEdit", path = "biomeRegion")
+    @OptionDescription(desc = "Maximum amount of chunks that a biomeRegion can have.") //TODO: Find out good amount
+    public int maxBiomeRegionChunks;
 
     public CCConfig() { }
 
