92894a1a1bb249b470812826b91f5a63b7a06ef3
seanlego23 <seanlego23@gmail.com>
2021-08-24 23:06:34 -0400

    Created BiomeRegionException.java

---
+++ src/main/java/io/github/seanlego23/customcompetitions/dependencies/worldedit/BiomeRegionException.java
@@ -0,0 +1,25 @@
+package io.github.seanlego23.customcompetitions.dependencies.worldedit;
+
+public class BiomeRegionException extends Exception {
+
+    public BiomeRegionException() {
+        super();
+    }
+
+    public BiomeRegionException(String message) {
+        super(message);
+    }
+
+    public BiomeRegionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public BiomeRegionException(Throwable cause) {
+        super(cause);
+    }
+
+    protected BiomeRegionException(String message, Throwable cause, boolean enableSuppression,
+            boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
