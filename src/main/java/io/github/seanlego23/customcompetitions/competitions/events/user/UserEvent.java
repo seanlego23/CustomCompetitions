9d59aa38aa38ead3d5250cee1d6c969242e51413
seanlego23 <seanlego23@gmail.com>
2021-08-27 19:52:06 -0400

    Created UserEvent.java

---
+++ src/main/java/io/github/seanlego23/customcompetitions/competitions/events/user/UserEvent.java
@@ -0,0 +1,20 @@
+package io.github.seanlego23.customcompetitions.competitions.events.user;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+public class UserEvent extends Event {
+
+    private static HandlerList handlers = new HandlerList();
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+
+    @NotNull
+    @Override
+    public HandlerList getHandlers() {
+        return getHandlerList();
+    }
+}
