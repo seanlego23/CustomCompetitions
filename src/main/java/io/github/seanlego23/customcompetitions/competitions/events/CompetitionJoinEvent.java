d0d1ffe7cd8595a33b7ec86cd337b975e1313f09
seanlego23 <seanlego23@gmail.com>
2021-08-27 19:46:58 -0400

    Moved CompetitionJoinEvent.java

--- src/main/java/io/github/seanlego23/customcompetitions/competitions/events/CompetitionJoinEvent.java
+++
@@ -1,4 +0,0 @@
-package io.github.seanlego23.customcompetitions.competitions.events;
-
-public class CompetitionJoinEvent {
-}
---
+++ src/main/java/io/github/seanlego23/customcompetitions/competitions/events/user/CompetitionJoinEvent.java
@@ -0,0 +1,7 @@
+package io.github.seanlego23.customcompetitions.competitions.events.user;
+
+public class CompetitionJoinEvent {
+
+
+
+}
