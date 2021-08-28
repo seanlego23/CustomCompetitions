3a5b64d7c4527094fa200d49bd488fe24333c6c6
seanlego23 <seanlego23@gmail.com>
2021-08-28 08:21:11 -0400

    Created CompetitionKickEvent.java

---
+++ src/main/java/io/github/seanlego23/customcompetitions/competitions/events/user/CompetitionKickEvent.java
@@ -0,0 +1,10 @@
+package io.github.seanlego23.customcompetitions.competitions.events.user;
+
+import io.github.seanlego23.customcompetitions.user.User;
+
+public class CompetitionKickEvent extends UserEvent {
+
+    public CompetitionKickEvent(User user) {
+        super(user);
+    }
+}
