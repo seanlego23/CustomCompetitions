c8c784c99d3f30fe58d8c4ecfe54355daae537ff
seanlego23 <seanlego23@gmail.com>
2021-08-28 08:13:13 -0400

    Added user variable

--- src/main/java/io/github/seanlego23/customcompetitions/competitions/events/user/UserEvent.java
+++ src/main/java/io/github/seanlego23/customcompetitions/competitions/events/user/UserEvent.java
@@ -1,12 +1,19 @@
 package io.github.seanlego23.customcompetitions.competitions.events.user;
 
+import io.github.seanlego23.customcompetitions.user.User;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
 
 public class UserEvent extends Event {
 
-    private static HandlerList handlers = new HandlerList();
+    private static final HandlerList handlers = new HandlerList();
+
+    protected final User user;
+
+    public UserEvent(User user) {
+        this.user = user;
+    }
 
     public static HandlerList getHandlerList() {
         return handlers;
@@ -16,5 +23,9 @@ public class UserEvent extends Event {
     @Override
     public HandlerList getHandlers() {
         return getHandlerList();
+    }
+
+    public User getUser() {
+        return user;
     }
 }
