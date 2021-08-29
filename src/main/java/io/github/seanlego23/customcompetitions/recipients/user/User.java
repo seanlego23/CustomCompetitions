9e660f493705ff00d577d9ce4ebcd73db1d37b5a
seanlego23 <seanlego23@gmail.com>
2021-08-29 12:24:24 -0400

    Moved User.java

---
+++ src/main/java/io/github/seanlego23/customcompetitions/recipients/user/User.java
@@ -0,0 +1,58 @@
+package io.github.seanlego23.customcompetitions.recipients.user;
+
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.BaseComponent;
+import org.bukkit.entity.Player;
+
+import java.util.UUID;
+
+public class User {
+    private final Player player;
+    private final String name;
+
+    public User(Player player) {
+        this.player = player;
+        name = player.getName();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean hasPermission(String perm) {
+        return player.hasPermission(perm);
+    }
+
+    public void sendMessage(BaseComponent component) {
+
+    }
+
+    public void sendMessage(BaseComponent... components) {
+
+    }
+
+    public void sendMessage(UUID sender, BaseComponent component) {
+
+    }
+
+    public void sendMessage(UUID sender, BaseComponent... components) {
+
+    }
+
+    public void sendMessage(ChatMessageType type, BaseComponent component) {
+
+    }
+
+    public void sendMessage(ChatMessageType type, BaseComponent... components) {
+
+    }
+
+    public void sendMessage(ChatMessageType type, UUID sender, BaseComponent component) {
+
+    }
+
+    public void sendMessage(ChatMessageType type, UUID sender, BaseComponent... components) {
+
+    }
+
+}
