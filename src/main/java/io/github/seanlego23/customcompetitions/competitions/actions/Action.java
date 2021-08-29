8bc446da8a6b435a7f8738c7e10f8b745e62b186
seanlego23 <seanlego23@gmail.com>
2021-08-29 11:57:02 -0400

    Added checkObjects method

--- src/main/java/io/github/seanlego23/customcompetitions/competitions/actions/Action.java
+++ src/main/java/io/github/seanlego23/customcompetitions/competitions/actions/Action.java
@@ -1,15 +1,30 @@
 package io.github.seanlego23.customcompetitions.competitions.actions;
 
 import io.github.seanlego23.customcompetitions.competitions.Competition;
+import org.jetbrains.annotations.NotNull;
 
 public interface Action {
 
     enum ActionResult {
-
+        NOT_ENOUGH_PARAMS,
+        INVALID_PARAMS,
+        SUCCESS,
     }
 
-    String name();
+    ActionResult perform(@NotNull Competition competition, Object... objects);
 
-    ActionResult perform(Competition competition, Object... objects);
+    static ActionResult checkObjects(Object[] objects, Class<?>... clz) {
+        if (clz.length == 0)
+            return ActionResult.SUCCESS;
+        if (objects.length < clz.length)
+            return ActionResult.NOT_ENOUGH_PARAMS;
+        for (int i = 0, j = 0; i < objects.length; i++) {
+            if (!clz[j].isAssignableFrom(objects[i].getClass()))
+                return ActionResult.INVALID_PARAMS;
+            if (j < clz.length - 1)
+                j++;
+        }
+        return ActionResult.SUCCESS;
+    }
 
 }
