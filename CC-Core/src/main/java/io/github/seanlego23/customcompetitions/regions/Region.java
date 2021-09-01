59934bd73d268ad41c00a4226b61dc89185f06d7
seanlego23 <seanlego23@gmail.com>
2021-09-01 08:22:36 -0400

    Add Region methods
    Added getWorld, getMinimum, getMaximum, expand, contract, and contains.

--- CC-Core/src/main/java/io/github/seanlego23/customcompetitions/regions/Region.java
+++ CC-Core/src/main/java/io/github/seanlego23/customcompetitions/regions/Region.java
@@ -1,4 +1,19 @@
 package io.github.seanlego23.customcompetitions.regions;
 
+import io.github.seanlego23.customcompetitions.util.math.Vector3D;
+
 public interface Region {
+
+    World getWorld();
+
+    Vector3D getMinimum();
+
+    Vector3D getMaximum();
+
+    void expand(Vector3D... changes);
+
+    void contract(Vector3D... changes);
+
+    boolean contains(Vector3D point);
+
 }
