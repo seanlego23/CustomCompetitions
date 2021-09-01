3b4c50d717f94da3ebcdb94b52ad43e9d1357aca
seanlego23 <seanlego23@gmail.com>
2021-09-01 08:23:11 -0400

    Implemented getWorld method

--- CC-Core/src/main/java/io/github/seanlego23/customcompetitions/regions/AbstractRegion.java
+++ CC-Core/src/main/java/io/github/seanlego23/customcompetitions/regions/AbstractRegion.java
@@ -1,4 +1,17 @@
 package io.github.seanlego23.customcompetitions.regions;
 
+import org.jetbrains.annotations.Nullable;
+
 public abstract class AbstractRegion implements Region {
+
+    protected World world;
+
+    public AbstractRegion(@Nullable World world) {
+        this.world = world;
+    }
+
+    @Override
+    public World getWorld() {
+        return world;
+    }
 }
