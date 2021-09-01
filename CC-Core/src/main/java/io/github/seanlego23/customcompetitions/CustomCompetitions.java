60bca6ee48b30d1e6403267b52568fc31f4f2208
seanlego23 <seanlego23@gmail.com>
2021-08-31 20:31:44 -0400

    Deleted dependency getter methods

--- CC-Core/src/main/java/io/github/seanlego23/customcompetitions/CustomCompetitions.java
+++ CC-Core/src/main/java/io/github/seanlego23/customcompetitions/CustomCompetitions.java
@@ -1,89 +1,22 @@
 package io.github.seanlego23.customcompetitions;
 
 import io.github.seanlego23.customcompetitions.competitions.CompetitionManager;
-import io.github.seanlego23.customcompetitions.dependencies.vault.VaultConnection;
-import io.github.seanlego23.customcompetitions.dependencies.worldedit.BiomeRegionCreator;
-import io.github.seanlego23.customcompetitions.dependencies.worldedit.WorldEditConnection;
-import io.github.seanlego23.customcompetitions.dependencies.worldguard.WorldGuardConnection;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.plugin.java.JavaPlugin;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
-import java.util.List;
-
-public final class CustomCompetitions extends JavaPlugin {
+public final class CustomCompetitions {
 
     private final CompetitionManager competitionManager = new CompetitionManager();
 
-    private final WorldEditConnection worldEditConnection = new WorldEditConnection(this);
-    private final WorldGuardConnection worldGuardConnection = new WorldGuardConnection(this);
-    private final VaultConnection vaultConnection = new VaultConnection(this);
-    private final BiomeRegionCreator biomeRegionCreator = new BiomeRegionCreator(this);
+//    private final BiomeRegionCreator biomeRegionCreator = new BiomeRegionCreator(this);
 
     public CustomCompetitions() {
 
-    }
-
-    @Override
-    public void onLoad() {
-        super.onLoad();
-
-        this.saveDefaultConfig();
-    }
-
-    @Override
-    public void onEnable() {
-        // Plugin startup logic
-
-    }
-
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
-
-    @Override
-    public void saveConfig() {
-        super.saveConfig();
-    }
-
-    @Override
-    public void saveDefaultConfig() {
-        super.saveDefaultConfig();
     }
 
     public CompetitionManager getCompetitionManager() {
         return competitionManager;
     }
 
-    public WorldEditConnection getWorldEditConnection() {
-        return worldEditConnection;
-    }
-
-    public WorldGuardConnection getWorldGuardConnection() {
-        return worldGuardConnection;
-    }
-
-    public VaultConnection getVaultConnection() {
-        return vaultConnection;
-    }
-
-    public BiomeRegionCreator getBiomeRegionCreator() {
+/*    public BiomeRegionCreator getBiomeRegionCreator() {
         return biomeRegionCreator;
-    }
-
-    @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label,
-            @NotNull String[] args) {
-        return false;
-    }
-
-    @Nullable
-    @Override
-    public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias,
-            @NotNull String[] args) {
-        return null;
-    }
+    }*/
 }
