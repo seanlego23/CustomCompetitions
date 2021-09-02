8edd3a30af3bc4a5bbbacd8d771e8fa4cf1db155
seanlego23 <seanlego23@gmail.com>
2021-09-01 22:57:01 -0400

    Give EllipsoidRegion a center and radius
    The radius for each direction can be combined into a three-dimensional vector as each corresponding component.

--- CC-Core/src/main/java/io/github/seanlego23/customcompetitions/regions/EllipsoidRegion.java
+++ CC-Core/src/main/java/io/github/seanlego23/customcompetitions/regions/EllipsoidRegion.java
@@ -1,4 +1,58 @@
 package io.github.seanlego23.customcompetitions.regions;
 
-public class EllipsoidRegion {
+import io.github.seanlego23.customcompetitions.util.math.Vector3D;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class EllipsoidRegion extends AbstractRegion {
+
+    private Vector3D center;
+    private Vector3D radius;
+
+    public EllipsoidRegion() {
+        super(null);
+    }
+
+    public EllipsoidRegion(@Nullable World world) {
+        super(world);
+    }
+
+    public EllipsoidRegion(@Nullable World world, @NotNull Vector3D center, @NotNull Vector3D radius) {
+        super(world);
+        this.center = center;
+        this.radius = radius;
+    }
+
+    public Vector3D getCenter() {
+        return center;
+    }
+
+    public Vector3D getRadius() {
+        return radius;
+    }
+
+    @Override
+    public Vector3D getMinimum() {
+        return null;
+    }
+
+    @Override
+    public Vector3D getMaximum() {
+        return null;
+    }
+
+    @Override
+    public void expand(Vector3D... changes) {
+
+    }
+
+    @Override
+    public void contract(Vector3D... changes) {
+
+    }
+
+    @Override
+    public boolean contains(Vector3D point) {
+        return false;
+    }
 }
