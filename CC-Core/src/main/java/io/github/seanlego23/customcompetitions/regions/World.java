72db54a1c6bd8893d1c8a250c4ab84b9b273320e
seanlego23 <seanlego23@gmail.com>
2021-09-01 22:02:33 -0400

    Change World to a Region interface

--- CC-Core/src/main/java/io/github/seanlego23/customcompetitions/regions/World.java
+++ CC-Core/src/main/java/io/github/seanlego23/customcompetitions/regions/World.java
@@ -1,4 +1,11 @@
 package io.github.seanlego23.customcompetitions.regions;
 
-public class World {
+public interface World extends Region {
+
+    String getName();
+
+    int getMaxHeight();
+
+    int getMinHeight();
+
 }
