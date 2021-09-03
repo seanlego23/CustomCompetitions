7320e10784136c019b19182b30008a38b788334a
seanlego23 <seanlego23@gmail.com>
2021-09-03 10:42:37 -0400

    Implement cardinal directions

--- CC-Core/src/main/java/io/github/seanlego23/customcompetitions/util/Direction.java
+++ CC-Core/src/main/java/io/github/seanlego23/customcompetitions/util/Direction.java
@@ -1,4 +1,38 @@
 package io.github.seanlego23.customcompetitions.util;
 
+import io.github.seanlego23.customcompetitions.util.math.Vector3D;
+
 public enum Direction {
+
+    SOUTH(0, 0, 1),
+    NORTH(0, 0, -1),
+    EAST(1, 0, 0),
+    WEST(-1, 0, 0);
+
+    private final int x;
+    private final int y;
+    private final int z;
+
+    Direction(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getZ() {
+        return z;
+    }
+
+    public Vector3D toVector3D() {
+        return new Vector3D(x, y, z);
+    }
+
 }
